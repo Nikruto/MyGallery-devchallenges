4e40c6c7e7e93500f965363e9e21f9a55f8b1842
Nikruto <nikruto1456@gmail.com>
2022-03-03 22:12:57 +0300

    Init

---
+++ README.md
@@ -0,0 +1,4 @@
+My Gallery - @devchallenges.io
+
+[Live](https://mygallery-nikruto.surge.sh/)\
+[Challenge](https://devchallenges.io/challenges/gcbWLxG6wdennelX7b8I)
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    fontFamily: {
+      sans: "Montserrat",
+    },
+    extend: {
+      colors: {
+        lightishGray: "#E5E5E5",
+        midGray: "#A9A9A9",
+        darkishGray: "#4E5150",
+        opaGray: "#BDBDBD",
+      },
+    },
+  },
+  plugins: [],
+};
